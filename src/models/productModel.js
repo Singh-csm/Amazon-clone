34c46f230a30b3c2a6369780e057524800149d0b
Tapaswee2001Samantaray <tapas10231@gmail.com>
2023-02-02 15:45:58 +0530

    product feature starts

---
+++ src/models/productModel.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    currencyId: {
+        type: String,
+        required: true,
+        default: "INR"
+    },
+    currencyFormat: {
+        type: String,
+        required: true,
+        default: "₹"
+    },
+    isFreeShipping: {
+        type: Boolean,
+        default: false
+    },
+    productImage: {
+        type: String,
+        required: true
+    },  // s3 link
+    style: {
+        type: String,
+        trim: true
+    },
+    availableSizes: {
+        type: String,
+        enum: ["S", "XS", "M", "X", "L", "XXL", "XL"]
+    },
+    installments: {
+        type: Number
+    },
+    deletedAt: {
+        type: Date
+    },
+    isDeleted: {
+        type: Boolean, default: false
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model("ProductData", productSchema);
